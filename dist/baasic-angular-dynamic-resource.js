f0fbb8c8be8c5f09adaac4ec1fe2eb75e204f28b
Kristijan Horvat <kristijan@mono.hr>
2014-10-30 16:15:30 +0100

    The bug related to wrong schema route is fixed

--- dist/baasic-angular-dynamic-resource.js
+++ dist/baasic-angular-dynamic-resource.js
@@ -52,10 +52,10 @@
         "use strict";
         module.service("baasicDynamicSchemaRouteService", ["baasicUriTemplateService", function (uriTemplateService) {
             return {
-                find: uriTemplateService.parse("schema/{?searchQuery,page,rpp,sort,embed,fields}"),
-                get: uriTemplateService.parse("schema/{resourceName}/{?embed,fields}"),
-                generate: uriTemplateService.parse("schema/generate"),
-                create: uriTemplateService.parse("schema"),
+                find: uriTemplateService.parse("schemas/{?searchQuery,page,rpp,sort,embed,fields}"),
+                get: uriTemplateService.parse("schemas/{resourceName}/{?embed,fields}"),
+                generate: uriTemplateService.parse("schemas/generate"),
+                create: uriTemplateService.parse("schemas"),
                 parse: uriTemplateService.parse
             };
         }]);
