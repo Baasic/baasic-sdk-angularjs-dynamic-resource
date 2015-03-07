9550b6de42106771d0870299c516faa35fe5b0c3
mzilic <mzilic@mono.hr>
2015-03-07 19:07:16 +0100

    - docs example updates

--- dist/baasic-angular-dynamic-resource.js
+++ dist/baasic-angular-dynamic-resource.js
@@ -72,7 +72,7 @@
                 /**
                  * Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [github](https://github.com/Baasic/uritemplate-js) page.
                  * @method
-                 * @example uriTemplateService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
+                 * @example baasicDynamicResourceRouteService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
                  **/
                 parse: uriTemplateService.parse,
                 permissions: {
@@ -105,7 +105,7 @@
                      * - `accessAction` - Action abbreviation which identifies ACL policy assigned to the specified role and article resource.
                      * - `role` - A value which uniquely identifies role for which ACL policy needs to be removed.					
                      * @method permissions.deleteByRole       
-                     * @example baasicArticleRatingsRouteService.permissions.deleteByRole.expand({schemaName: "schemaName", id: "resourceId", accessAction: "read", role: "roleName"});               
+                     * @example baasicDynamicResourceRouteService.permissions.deleteByRole.expand({schemaName: "schemaName", id: "resourceId", accessAction: "read", role: "roleName"});               
                      **/
                     deleteByRole: uriTemplateService.parse("resources/{schemaName}/{id}/permissions/actions/{accessAction}/roles/{role}/")
                 }
@@ -366,7 +366,7 @@
                 /**
                  * Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [github](https://github.com/Baasic/uritemplate-js) page.
                  * @method
-                 * @example uriTemplateService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
+                 * @example baasicDynamicSchemaRouteService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
                  **/
                 parse: uriTemplateService.parse
             };
--- src/services/dynamicResourceRouteService.js
+++ src/services/dynamicResourceRouteService.js
@@ -41,7 +41,7 @@
 				/**
 				* Parses and expands URI templates based on [RFC6570](http://tools.ietf.org/html/rfc6570) specifications. For more information please visit the project [github](https://github.com/Baasic/uritemplate-js) page.
 				* @method
-				* @example uriTemplateService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
+				* @example baasicDynamicResourceRouteService.parse("route/{?embed,fields,options}").expand({embed: "embeddedResource"});
 				**/					
                 parse: uriTemplateService.parse,					
                 permissions: {
@@ -74,7 +74,7 @@
 					* - `accessAction` - Action abbreviation which identifies ACL policy assigned to the specified role and article resource.
 					* - `role` - A value which uniquely identifies role for which ACL policy needs to be removed.					
 					* @method permissions.deleteByRole       
-					* @example baasicArticleRatingsRouteService.permissions.deleteByRole.expand({schemaName: "schemaName", id: "resourceId", accessAction: "read", role: "roleName"});               
+					* @example baasicDynamicResourceRouteService.permissions.deleteByRole.expand({schemaName: "schemaName", id: "resourceId", accessAction: "read", role: "roleName"});               
 					**/ 					
                     deleteByRole: uriTemplateService.parse("resources/{schemaName}/{id}/permissions/actions/{accessAction}/roles/{role}/")
                 }
