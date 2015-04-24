2feb3a4ab99293b3721d8da7c76ca49a382f7b7d
Dijana <dijana@mono.hr>
2015-04-24 15:11:51 +0200

    Removed footer references

--- src/services/dynamicResourceRouteService.js
+++ src/services/dynamicResourceRouteService.js
@@ -109,9 +109,6 @@ baasicDynamicResourceRouteService.acl.deleteByRole.expand({
         }]);
 }(angular, module));
 /**
- * @copyright (c) 2015 Mono
- * @license MIT
- * @author Mono
  * @overview 
  ***Notes:**
  - Refer to the [REST API documentation](https://github.com/Baasic/baasic-rest-api/wiki) for detailed information about available Baasic REST API end-points.
